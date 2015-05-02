dc235d54f3cefc14cae91f412c1c20b4e84e051e
elpipiripao <luispablomoralescruz@MacBook-Pro-de-Luis.local>
2015-05-01 19:28:02 -0500

    lol

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,11 +20,11 @@ class Welcome extends CI_Controller {
 	 */
     public function marker(){
         $this->load->library("googlemaps");
-        $config["center"] = "37.4419, -122.1419";
+        $config["center"] = "20.613051, -100.405856";
         $config["zoom"] = "auto";
         $this->googlemaps->initialize($config);
         $marker = array();
-        $marker["position"] = "37.429, -122.1419";
+        $marker["position"] = "20.613051, -100.405856";
         $this->googlemaps->add_marker($marker);
         $data["map"] = $this->googlemaps->create_map();
         $this->load->view("marker", $data);
