e8575f2b016b3acb2cf4c1ff48377d8bc38e17e8
elpipiripao <luispablomoralescruz@MacBook-Pro-de-Luis.local>
2015-05-04 12:50:43 -0500

    Revert 76630fe..94f7be4
    This rolls back to commit 76630fe2923f23cf1e6d1fbba981fbe322df73e7.

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,7 +3,6 @@
 
     class Welcome extends CI_Controller {
 
-<<<<<<< Updated upstream
         /**
          * Index Page for this controller.
          *
@@ -44,55 +43,6 @@
         // Stashed changes
         public function map(){
             $this->load->library("googlemaps");
-=======
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-    public function marker(){
-        $this->load->library("googlemaps");
-        $config["center"] = "20.596202, -100.403871";
-        $config["zoom"] = "auto";
-        $this->googlemaps->initialize($config);
-        $marker = array();
-        $marker["position"] = "20.596202, -100.403871";
-        $config["center"] = "20.613051, -100.405856";
-        $config["zoom"] = "auto";
-        $this->googlemaps->initialize($config);
-        $marker = array();
-        $marker["position"] = "20.613051, -100.405856";
-        $this->googlemaps->add_marker($marker);
-        $data["map"] = $this->googlemaps->create_map();
-        $this->load->view("marker", $data);
-    }
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
-    
-    public function directions()
-    {
-     $this->load->library("googlemaps");
-     $config["center"] = "20.596202, -100.403871";
-     $config["zoom"] = "auto";
-     $config["directions"] = TRUE;
-     $config["directionsStart"] = "empire state building";
-     $config["directionsEnd"] = "statue of liberty";
-     $config["directionsDivID"] = "directionsDiv";
-     $this->googlemaps->initialize($config);
-     $data["map"] = $this->googlemaps->create_map();
->>>>>>> Stashed changes
 
             $config = array();
             $config["center"] = "auto";
@@ -151,39 +101,6 @@
             $data["map"] = $this->googlemaps->create_map();
             $this->load->view("map", $data);
          }
-      $config = array();
-      $config['center'] = 'auto';
-      $config['onboundschanged'] = 'if (!centreGot) {
-      var mapCentre = map.getCenter();
-      marker_0.setOptions({
-      position: new google.maps.LatLng(mapCentre.lat(),      mapCentre.lng()) 
-      });
-      }
-      centreGot = true;';
-      $this->googlemaps->initialize($config);
-
-      // set up the marker ready for positioning 
-      // once we know the users location
-      $marker = array();
-      $this->googlemaps->add_marker($marker);
-      
-      $config['styles'] = array(
-    array("name"=>"Purple Parks", "definition"=>array(
-    array("featureType"=>"all", "stylers"=>array(array("saturation"=>"-30"))),
-    array("featureType"=>"poi.park", "stylers"=>array(array("saturation"=>"10"), array("hue"=>"#4F0A95")))
-  )),
-  array("name"=>"Black Roads", "definition"=>array(
-    array("featureType"=>"all", "stylers"=>array(array("saturation"=>"-70"))),
-    array("featureType"=>"road.arterial", "elementType"=>"geometry", "stylers"=>array(array("hue"=>"#049BA9")))
-  )),
-  array("name"=>"No Businesses", "definition"=>array(
-    array("featureType"=>"poi.business", "elementType"=>"labels", "stylers"=>array(array("visibility"=>"off")))
-  ))
-);
-        $config['stylesAsMapTypes'] = true;
-        $config['stylesAsMapTypesDefault'] = "Black Roads"; 
-        $this->googlemaps->initialize($config);
-        $data["map"] = $this->googlemaps->create_map();
 
         public function Nuevos_Huertos(){
             $this->load->library('googlemaps');
